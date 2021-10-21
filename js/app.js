e0374b57f83a62bfa04cda8576b6dc0397142a52
Paulino101 <89439038+Paulino101@users.noreply.github.com>
2021-10-21 13:25:12 -0700

    touched up mobile view

--- js/app.js
+++ js/app.js
@@ -10,97 +10,145 @@ for (i = 0; i < close.length; i++) {
     }, 600);
   };
 }
+///alert section///
+const alertBanner = document.getElementById("alert");
+// create the html for the banner
+alertBanner.innerHTML = `
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`;
+alertBanner.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+  }
+});
+///MEssaging Section///
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
 
-// LIne Chart//
+send.addEventListener("click", () => {
+  // ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
+});
 
-// const labels = ["January", "February", "March", "April", "May", "June"];
-// const data = {
-//   labels: labels,
-//   datasets: [
-//     {
-//       label: "My First dataset",
-//       backgroundColor: "rgb(255, 99, 132)",
-//       borderColor: "rgb(255, 99, 132)",
-//       data: [0, 10, 5, 2, 20, 30, 45],
-//     },
-//   ],
-// };
-
-// const config = {
-//   type: "line",
-//   data: data,
-//   options: {},
-// };
-// // === include 'setup' then 'config' above ===
-
-// var myChart = new Chart(document.getElementById("myChart"), config);
-//
-
-// Bar Chart //
-const labels = ["S", "M", "T", "W", "T", "F", "S"];
-const data = {
-  labels: labels,
+// Line Chart//
+const trafficCanvas = document.getElementById("traffic-chart");
+let trafficData = {
+  labels: [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-17",
+    "18-24",
+    "25-31",
+  ],
   datasets: [
     {
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(255, 205, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(201, 203, 207, 0.2)",
-      ],
-      borderColor: [
-        "rgb(255, 99, 132)",
-        "rgb(255, 159, 64)",
-        "rgb(255, 205, 86)",
-        "rgb(75, 192, 192)",
-        "rgb(54, 162, 235)",
-        "rgb(153, 102, 255)",
-        "rgb(201, 203, 207)",
-      ],
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: "rgba(116, 119, 191, .3)",
       borderWidth: 1,
     },
   ],
 };
-const config = {
+let trafficOptions = {
+  backgroundColor: "rgba(112, 104, 201, .5)",
+  fill: true,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0,
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+let trafficChart = new Chart(trafficCanvas, {
+  type: "line",
+  data: trafficData,
+  options: trafficOptions,
+});
+// // Bar Chart //
+const dailyCanvas = document.getElementById("daily-chart");
+// data for daily traffic bar chart
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      label: "# of Hits",
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: "#7477BF",
+      borderWidth: 1,
+    },
+  ],
+};
+const dailyOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+let dailyChart = new Chart(dailyCanvas, {
   type: "bar",
-  data: data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+  data: dailyData,
+  options: dailyOptions,
+});
+//doughnut chart//
+const mobileCanvas = document.getElementById("mobile-chart");
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      label: "# of Users",
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"],
+    },
+  ],
+};
+const mobileOptions = {
+  aspectRatio: 1.9,
+  plugins: {
+    legend: {
+      position: "right",
+      labels: {
+        boxWidth: 20,
+        fontStyle: "bold",
       },
     },
   },
 };
-// === include 'setup' then 'config' above ===
-
-var myChart = new Chart(document.getElementById("myChart"), config);
-
-//////////////////
-//Doughnut Chart//
-const dataDoughnut = {
-  labels: ["Desktop", "Tablet", "Phones"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [300, 50, 100],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
-      hoverOffset: 4,
-    },
-  ],
-};
-const configDoughnut = {
+let mobileChart = new Chart(mobileCanvas, {
   type: "doughnut",
-  data: dataDoughnut,
-};
-// === include 'setup' then 'config' above ===
-
-var myChart = new Chart(document.getElementById("doughnutChart"), config);
+  data: mobileData,
+  options: mobileOptions,
+});
