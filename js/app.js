fdcc933ec4f5d3966bb0d497d8cecf804fa8d50e
Paulino101 <89439038+Paulino101@users.noreply.github.com>
2021-10-22 12:54:35 -0700

    formatted all view ports

--- js/app.js
+++ js/app.js
@@ -15,8 +15,7 @@ const alertBanner = document.getElementById("alert");
 // create the html for the banner
 alertBanner.innerHTML = `
 <div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
+<p><strong>Alert:</strong> You have unread messages</p>
 <p class="alert-banner-close">x</p>
 </div>
 `;
